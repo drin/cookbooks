ca052d46671038363151fe8d3e35b6da78229fc0
Aldrin <akmontan@ucsc.edu>
2022-04-22 15:53:44 -0700

    changed datatypes.hpp -> recipe.hpp
    This is a common header for this recipe. Some fn prototypes added for
    storage.cpp, added some extra `using` statements, and added some other
    fn prototypes.

---
+++ arrow/ipc-dict-array/recipe.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+// ------------------------------
+// Dependencies
+
+// standard dependencies
+#include <stdint.h>
+#include <string>
+#include <iostream>
+
+// arrow dependencies
+#include <arrow/api.h>
+#include <arrow/ipc/api.h>
+#include <arrow/dataset/api.h>
+#include <arrow/compute/api.h>
+
+
+// ------------------------------
+// Aliases
+
+// aliases for standard types
+using std::string;
+using std::shared_ptr;
+using std::vector;
+
+// arrow util types
+using arrow::Result;
+using arrow::Status;
+
+// arrow data types
+using arrow::DataType;
+using arrow::Array;
+using arrow::StringArray;
+using arrow::DictionaryArray;
+
+using arrow::ChunkedArray;
+using arrow::Table;
+using arrow::Schema;
+
+using arrow::ipc::RecordBatchWriter;
+using arrow::ipc::RecordBatchStreamReader;
+
+// complex options for IPC readers and writers
+using arrow::ipc::IpcReadOptions;
+using arrow::ipc::IpcWriteOptions;
+
+// arrow compute functions
+using arrow::compute::DictionaryEncode;
+
+
+// ------------------------------
+// Functions
+
+// recipe functions (interacting with DictionaryArray)
+Result<shared_ptr<StringArray>>
+ConstructStrArray(vector<string> src_vector);
+
+Result<shared_ptr<DictionaryArray>>
+DictArrFromVal(vector<string> arr_vals);
+
+Result<shared_ptr<Table>>
+ConstructTestTable();
+
+
+// storage functions (readers and writers)
+string ConstructFileUri(char *file_dirpath);
+
+Result<shared_ptr<RecordBatchWriter>>
+WriterForIPCFile(shared_ptr<Schema> schema, const string &path_as_uri);
+
+Result<shared_ptr<RecordBatchStreamReader>>
+ReaderForIPCFile(const std::string &path_as_uri);
