189e3cd7245b01fca3d6229a85d4e249a0a7abc1
Aldrin M <octalene.dev@pm.me>
2022-06-29 18:10:32 -0700

    add-compute-fn: updated compute function
    This compute function now wraps the existing hash function
    implementation and has the bare minimum to be functional for a primitive
    inputs.
    
    This should be improved to do some templating and metaprogramming to
    show how to accommodate primitive types vs binary types and nested
    types.

--- arrow/add-compute-fn/recipe.cc
+++ arrow/add-compute-fn/recipe.cc
@@ -1,103 +1,124 @@
+#pragma once
+
+
 // ------------------------------
 // Dependencies
 
-// Local and third-party dependencies
 #include "recipe.hpp"
 
+
 // ------------------------------
-// Macros and aliases
+// Structs and Classes
 
-//  |> documentation
-
-/*
- * Documentation instances have 3 attributes:
+// >> Documentation for a compute function
+/**
+ * Create a const instance of `FunctionDoc` that contains 3 attributes:
  *  1. Short description
- *  2. Long  description
+ *  2. Long  description (limited to 78 characters)
  *  3. Name of input arguments
  */
 const FunctionDoc named_scalar_fn_doc {
-   "Calculate hashes for each element of the input argument"
-  ,(
-     "Calculate hash values using an algorithm based on xxHash. "
-     // "This returns a hash value corresponding to each row of the input."
-   )
+   "Unary function that calculates a hash for each row of the input"
+  ,"This function uses an xxHash-like algorithm which produces 32-bit hashes."
   ,{ "input_array" }
 };
 
 
-// ------------------------------
-// Functions
-
+// >> Kernel implementations for a compute function
 /**
- * A C++ function that invokes our compute function from the function registry via
- * `CallFunction`. This allows us to register our compute function as `func_name` (e.g.
- * "named_scalar_fn") but call it as if it was typically defined (e.g.
- * `NamedScalarFn()`).
- */
-ARROW_EXPORT
-Result<Datum>
-NamedScalarFn(const Datum &input_arg, ExecContext *ctx) {
-  auto func_name = "named_scalar_fn";
-  return CallFunction(func_name, { input_arg }, ctx);
-}
-
-
-/**
- * Kernel definitions (internal linkage).
+ * Create implementations that will be associated with our compute function. When a
+ * compute function is invoked, the compute API framework will delegate execution to an
+ * associated kernel that matches: (1) input argument types/shapes and (2) output argument
+ * types/shapes.
  *
- * These are implementations of the compute function for specific data types and data
- * shapes of input arguments. For example, we have 1 kernel that takes a scalar data type
- * (single element), and we have a different kernel that takes an array data type (many
- * elements).
+ * Kernel implementations may be functions or may be methods (functions within a class or
+ * struct).
  */
 struct NamedScalarFn {
+
+  /**
+   * A kernel implementation that expects a single array as input, and outputs an array of
+   * uint32 values. We write this implementation knowing what function we want to
+   * associate it with ("NamedScalarFn"), but that association is made later (see
+   * `RegisterScalarFnKernels()` below).
+   */
   static Status
   Call(KernelContext *ctx, const ExecSpan &input_arg, ExecResult *out) {
-    // auto size_tmpstack = 64 * std::min(input_array.length(), max_batchsize);
     ARROW_LOG(INFO) << "Calling kernel 'NamedScalarFn'";
     if (input_arg.num_values() != 1 or not input_arg[0].is_array()) {
       return Status::Invalid("Unsupported argument types or shape");
     }
 
+    // >> Initialize stack-based memory allocator with an allocator and memory size
     ARROW_LOG(INFO) << "Accessing input data";
-    shared_ptr<Int32Array> input_arr = std::static_pointer_cast<Int32Array>(input_arg[0].array.ToArray());
-    const int32_t *arr_data          = input_arr->raw_values();
 
-    ARROW_LOG(INFO) << "Hashing inputs";
-    std::hash<int32_t>   default_hash;
-    std::vector<int32_t> hash_results;
-    hash_results.reserve(input_arr->length());
-    for (int64_t arr_ndx = 0; arr_ndx < input_arr->length(); ++arr_ndx) {
-      hash_results.push_back(
-        (int32_t) default_hash(arr_data[arr_ndx])
+    TempVectorStack stack_memallocator;
+    auto            input_dtype_width = input_arg[0].type()->bit_width();
+    if (input_dtype_width > 0) {
+      ARROW_RETURN_NOT_OK(
+        stack_memallocator.Init(
+           ctx->exec_context()->memory_pool()
+          ,input_dtype_width * max_batchsize
+        )
       );
     }
 
+    // >> Prepare input data structure for propagation to hash function
+    // NOTE: "start row index" and "row count" can potentially be options in the future
+    ARROW_LOG(INFO) << "Accessing input data";
+
+    ArraySpan hash_input    = input_arg[0].array;
+    int64_t   hash_startrow = 0;
+    int64_t   hash_rowcount = hash_input.length;
+    ARROW_ASSIGN_OR_RAISE(
+       KeyColumnArray input_keycol
+      ,ColumnArrayFromArrayData(hash_input.ToArrayData(), hash_startrow, hash_rowcount)
+    );
+
+    // >> Call hashing function
+    ARROW_LOG(INFO) << "Calling hash function";
+    vector<uint32_t> hash_results;
+    hash_results.resize(hash_input.length);
+
+    LightContext hash_ctx;
+    hash_ctx.hardware_flags = ctx->exec_context()->cpu_info()->hardware_flags();
+    hash_ctx.stack          = &stack_memallocator;
+
+    Hashing32::HashMultiColumn({ input_keycol }, &hash_ctx, hash_results.data());
+
+    // >> Prepare results of hash function for kernel output argument
     ARROW_LOG(INFO) << "Moving hash results to an Array";
-    Int32Builder builder;
-    builder.Reserve(input_arr->length());
+    UInt32Builder builder;
+    builder.Reserve(hash_results.size());
     builder.AppendValues(hash_results);
 
-    ARROW_LOG(INFO) << "Finishing array";
     ARROW_ASSIGN_OR_RAISE(auto result_array, builder.Finish());
-
-    ARROW_LOG(INFO) << "Setting kernel outputs";
     out->value = result_array->data();
 
     ARROW_LOG(INFO) << "Kernel execution complete";
     return Status::OK();
   }
 
+
   static constexpr uint32_t max_batchsize = MiniBatch::kMiniBatchLength;
 };
 
 
 // ------------------------------
-// Registration
+// Functions
 
+
+// >> Function registration and kernel association
 /**
- * A convenience function that constructs a `ScalarFunction` instance named
- * "absolute_value" and registers unchecked versions of the AbsoluteValue kernel.
+ * A convenience function that shows how we construct an instance of `ScalarFunction` that
+ * will be registered in a function registry. The instance is constructed with: (1) a
+ * unique name ("named_scalar_fn"), (2) an "arity" (`Arity::Unary()`), and (3) an instance
+ * of `FunctionDoc`.
+ *
+ * The function name is used to invoke it from a function registry after it has been
+ * registered. The "arity" is the cardinality of the function's parameters--1 parameter is
+ * a unary function, 2 parameters is a binary function, etc. Finally, it is helpful to
+ * associate the function with documentation, which uses the `FunctionDoc` struct.
  */
 shared_ptr<ScalarFunction>
 RegisterScalarFnKernels() {
@@ -108,11 +129,12 @@ RegisterScalarFnKernels() {
     ,std::move(named_scalar_fn_doc)
   );
 
-  // Register a kernel for each data type we want this function to accommodate
+  // Associate a kernel implementation with the function using
+  // `ScalarFunction::AddKernel()`
   DCHECK_OK(
     fn_named_scalar->AddKernel(
        { InputType(arrow::int32()) }
-      ,OutputType(arrow::int32())
+      ,OutputType(arrow::uint32())
       ,NamedScalarFn::Call
     )
   );
@@ -122,13 +144,27 @@ RegisterScalarFnKernels() {
 
 
 /**
- * A convenience function that takes a `FunctionRegistry` pointer as an input argument.
- *
- * This function calls 2 other convenience functions, then registers the returned
- * `ScalarFunction` instance from each in the input `FunctionRegistry`.
+ * A convenience function that shows how we register a custom function with a
+ * `FunctionRegistry`. To keep this simple and general, this function takes a pointer to a
+ * FunctionRegistry as an input argument, then invokes `FunctionRegistry::AddFunction()`.
  */
 void
 RegisterNamedScalarFn(FunctionRegistry *registry) {
   auto scalar_fn = RegisterScalarFnKernels();
   DCHECK_OK(registry->AddFunction(std::move(scalar_fn)));
 }
+
+
+// >> Convenience functions
+/**
+ * An optional convenience function to easily invoke our compute function. This executes
+ * our compute function by invoking `CallFunction` with the name that we used to register
+ * the function ("named_scalar_fn" in this case).
+ */
+ARROW_EXPORT
+Result<Datum>
+NamedScalarFn(const Datum &input_arg, ExecContext *ctx) {
+  auto func_name = "named_scalar_fn";
+  return CallFunction(func_name, { input_arg }, ctx);
+}
+
--- arrow/add-compute-fn/recipe.hpp
+++ arrow/add-compute-fn/recipe.hpp
@@ -4,57 +4,21 @@
 // ------------------------------
 // Dependencies
 
-// consolidated dependencies to keep this header concise
 #include "support.hpp"
-
-// STL
-#include <functional>
-
-
-// ------------------------------
-// Macros and Aliases
-
-using std::hash;
 
 
 // ------------------------------
 // Functions
 
-// template functions
-
-template<typename DataType, typename ArrayType>
-arrow::enable_if_base_binary<DataType, int64_t>
-CalculateTempStackSize(shared_ptr<ArrayType> sample_col) {
-  auto arr_slice = std::static_pointer_cast<ArrayType>(
-    sample_col->Slice(0, MiniBatch::kMiniBatchLength)
-  );
-
-  return 64 * arr_slice->total_values_length();
-}
-
-template<typename DataType, typename ArrayType>
-arrow::enable_if_primitive_ctype<DataType, int64_t>
-CalculateTempStackSize(shared_ptr<ArrayType> sample_col) {
-  return 64 * std::min(sample_col->length(), MiniBatch::kMiniBatchLength);
-}
-
-
-// ------------------------------
-// Recipes for named functions
-
-
-// >> Functions
-
-/** A named function (visible in a "translation unit") that we will register. */
-ARROW_EXPORT
-Result<Datum>
-NamedScalarFn(const Datum &input_arg, ExecContext *ctx = NULLPTR);
-
+// >> Function registration and kernel association
 /** A convenience function that registers our new, named function. */
 ARROW_EXPORT
 void
 RegisterNamedScalarFn(FunctionRegistry *registry);
 
 
-// ------------------------------
-// Recipes for anonymous functions
+// >> Convenience functions
+/** A named function (visible in a "translation unit") that we will register. */
+ARROW_EXPORT
+Result<Datum>
+NamedScalarFn(const Datum &input_arg, ExecContext *ctx = NULLPTR);
